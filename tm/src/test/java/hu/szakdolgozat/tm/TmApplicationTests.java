77fed9b97527d328c1f7627c7af7080d5b09368c
Martin <fejtimartin98@gmail.com>
2020-10-13 16:29:14 +0200

    - Add basic project to repo with established db connection

---
+++ tm/src/test/java/hu/szakdolgozat/tm/TmApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szakdolgozat.tm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
