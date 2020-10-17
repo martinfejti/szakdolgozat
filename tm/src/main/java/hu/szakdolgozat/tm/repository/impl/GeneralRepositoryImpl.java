fbb6558a5c6635f0d2c5cdfb46fa40068489a6bc
Martin <fejtimartin98@gmail.com>
2020-10-17 13:50:09 +0200

    - Add transactional annotation to GeneralRepositoryImpl

--- tm/src/main/java/hu/szakdolgozat/tm/repository/impl/GeneralRepositoryImpl.java
+++ tm/src/main/java/hu/szakdolgozat/tm/repository/impl/GeneralRepositoryImpl.java
@@ -2,11 +2,13 @@ package hu.szakdolgozat.tm.repository.impl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 import hu.szakdolgozat.tm.repository.GeneralRepository;
 
+@Transactional
 @Repository
 public class GeneralRepositoryImpl implements GeneralRepository {
 
