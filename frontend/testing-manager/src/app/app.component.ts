c6cb8f46a7780a5352d531d11fba2c1aae141879
Martin <fejtimartin98@gmail.com>
2020-10-20 19:06:21 +0200

    - Add basic function to decide which view should be open

--- frontend/testing-manager/src/app/app.component.ts
+++ frontend/testing-manager/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testing-manager';
+  selectedView: string;
+
+  constructor() {
+    this.selectedView = 'PROJECT_DETAILS';
+  }
+
+  changeSelectedView(type: 'PROJECT_DETAILS' | 'COMPONENT_DETAILS' | 'CASE_DETAILS' | 'RUN_TEST' | 'USERS_GUIDE' | undefined) {
+    this.selectedView = type;
+  }
 }
---
+++ frontend/testing-manager/src/models/selected-view.ts
@@ -0,0 +1,5 @@
+export interface SelectedView {
+    type: 'PROJECT_DETAILS' | 'COMPONENT_DETAILS' | 'CASE_DETAILS' | 'RUN_TEST' | 'USERS_GUIDE' | undefined;
+}
+
+
