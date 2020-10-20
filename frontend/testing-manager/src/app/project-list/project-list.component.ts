90754114ff29ca9fba7663eed233a2579698b622
Martin <fejtimartin98@gmail.com>
2020-10-20 21:16:28 +0200

    - Show project list elements TEMP

--- frontend/testing-manager/src/app/project-list/project-list.component.ts
+++ frontend/testing-manager/src/app/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from './../../models/project';
 
 @Component({
   selector: 'app-project-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
+  @Input() projects: Project[];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.projects);
   }
 
 }
