ebc520ec7a97ca974e4b93eaeb6aa7a05408fe7c
Martin <fejtimartin98@gmail.com>
2020-10-28 14:08:34 +0100

    - Open new test component

--- frontend/testing-manager/src/app/component-test/component-test.component.ts
+++ frontend/testing-manager/src/app/component-test/component-test.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Component as ComponentModel } from './../../models/component';
 
 @Component({
   selector: 'app-component-test',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./component-test.component.scss']
 })
 export class ComponentTestComponent implements OnInit {
+
+  @Input() component: ComponentModel;
 
   constructor() { }
 
