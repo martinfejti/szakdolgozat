de92ce6dd94c536eb6783c2aa2dee26d759d0652
Martin <fejtimartin98@gmail.com>
2020-10-29 19:52:59 +0100

    - Create constants util class in backend

---
+++ tm/src/main/java/hu/szakdolgozat/tm/utils/Constants.java
@@ -0,0 +1,11 @@
+package hu.szakdolgozat.tm.utils;
+
+public interface Constants {
+
+    interface ErrorMessages {
+            String QUERY_FAILED = "Query has failed!";
+            String CREATE_FAILED = "Create has failed!";
+            String UPDATE_FAILED = "Update has failed!";
+            String DELETE_FAILED = "Delete has failed!";
+    }
+}
