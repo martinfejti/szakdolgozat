88cedafd4d696d6e6b3dedd928de3793a91e4519
Martin <fejtimartin98@gmail.com>
2020-10-30 21:24:04 +0100

    - Order code

--- tm/src/main/java/hu/szakdolgozat/tm/service/impl/ExcelServiceImpl.java
+++ tm/src/main/java/hu/szakdolgozat/tm/service/impl/ExcelServiceImpl.java
@@ -48,32 +48,9 @@ public class ExcelServiceImpl implements ExcelService {
             ProjectEntity projectEntity = this.projectRepository.getProjectById(id);
             
             XSSFWorkbook workbook = new XSSFWorkbook();
-            
             XSSFSheet projectSheet = workbook.createSheet("Project details");
             
-            Row nameRow = projectSheet.createRow(0);
-            Cell nameLabelCell = nameRow.createCell(0);
-            nameLabelCell.setCellValue("Project name:");
-            
-            Cell nameCell = nameRow.createCell(1);
-            nameCell.setCellValue(projectEntity.getName());
-            
-            Row shortDescriptionRow = projectSheet.createRow(1);
-            Cell shortDescriptionLabelCell = shortDescriptionRow.createCell(0);
-            shortDescriptionLabelCell.setCellValue("Project short description:");
-            
-            Cell shortDescriptionCell = shortDescriptionRow.createCell(1);
-            shortDescriptionCell.setCellValue(projectEntity.getShortDescription());
-            
-            Row longDescriptionRow = projectSheet.createRow(2);
-            Cell longDescriptionLabelCell = longDescriptionRow.createCell(0);
-            longDescriptionLabelCell.setCellValue("Project long description:");
-            
-            Cell longDescriptionCell = longDescriptionRow.createCell(1);
-            longDescriptionCell.setCellValue(projectEntity.getLongDescription());
-            
-            projectSheet.setColumnWidth(0, 10000);
-            projectSheet.setColumnWidth(1, 10000);
+            fillProjectDetailsPage(projectEntity, projectSheet);
             
             if (projectEntity.getComponentEntities() != null && !projectEntity.getComponentEntities().isEmpty()) {
                 for (int i = 0; i < projectEntity.getComponentEntities().size(); i++) {
@@ -91,45 +68,12 @@ public class ExcelServiceImpl implements ExcelService {
                     style.setBorderLeft(BorderStyle.THICK);
                     style.setBorderRight(BorderStyle.THICK);
                     style.setBorderTop(BorderStyle.THICK);
-
-                    Cell cell0 = mainRow.createCell(0);
-                    cell0.setCellValue(CASE_ID);
-                    cell0.setCellStyle(style);
-                    
-                    Cell cell1 = mainRow.createCell(1);
-                    cell1.setCellValue(CASE_NAME);
-                    cell1.setCellStyle(style);
-                    
-                    Cell cell2 = mainRow.createCell(2);
-                    cell2.setCellValue(CASE_DESCRIPTION);
-                    cell2.setCellStyle(style);
-                    
-                    Cell cell3 = mainRow.createCell(3);
-                    cell3.setCellValue(STEP_NUMBER);
-                    cell3.setCellStyle(style);
-                    
-                    Cell cell4 = mainRow.createCell(4);
-                    cell4.setCellValue(STEP_DESCRIPTION);
-                    cell4.setCellStyle(style);
-                    
-                    Cell cell5 = mainRow.createCell(5);
-                    cell5.setCellValue(STEP_EXPECTED_RESULT);
-                    cell5.setCellStyle(style);
-
-                    Cell cell6 = mainRow.createCell(6);
-                    cell6.setCellValue(STEP_COMMENT);
-                    cell6.setCellStyle(style);
                     
                     CellStyle borderStyle = workbook.createCellStyle();
                     borderStyle.setBorderTop(BorderStyle.DOUBLE);
-                    
-                    sheet.setColumnWidth(0, 2000);
-                    sheet.setColumnWidth(1, 8000);
-                    sheet.setColumnWidth(2, 10000);
-                    sheet.setColumnWidth(3, 2000);
-                    sheet.setColumnWidth(4, 10000);
-                    sheet.setColumnWidth(5, 10000);
-                    sheet.setColumnWidth(6, 6000);
+
+                    fillMainRow(mainRow, style);
+                    setSheetHeader(sheet);
                     
                     // create other rows
                     for (int j = 0; j < componentEntity.getCaseEntities().size(); j++) {
@@ -151,26 +95,7 @@ public class ExcelServiceImpl implements ExcelService {
                             StepEntity stepEntity = caseEntity.getSteps().get(k);
                             
                             if (k == 0) {
-                                Cell stepNumberCell = row.createCell(3);
-                                stepNumberCell.setCellValue(stepEntity.getOrderNumber());
-                                stepNumberCell.setCellStyle(borderStyle);
-                                
-                                Cell stepDescriptionCell = row.createCell(4);
-                                stepDescriptionCell.setCellValue(stepEntity.getDescription());
-                                stepDescriptionCell.setCellStyle(borderStyle);
-                                
-                                Cell stepExpectedResultCell = row.createCell(5);
-                                stepExpectedResultCell.setCellValue(stepEntity.getExpectedResult());
-                                stepExpectedResultCell.setCellStyle(borderStyle);
-                                
-                                Cell stepCommentCell = row.createCell(6);
-                                if (stepEntity.getComment() != null) {
-                                    stepCommentCell.setCellValue(stepEntity.getComment());
-                                    stepCommentCell.setCellStyle(borderStyle);
-                                } else {
-                                    stepCommentCell.setCellValue("-");
-                                    stepCommentCell.setCellStyle(borderStyle);
-                                } 
+                                createStepRows(row, borderStyle, stepEntity);
                             } else {
                                 Row stepRow = sheet.createRow(j + 1 + k);
                                 Cell stepNumberCell = stepRow.createCell(3);
@@ -217,6 +142,95 @@ public class ExcelServiceImpl implements ExcelService {
         } catch (PersistenceException e) {
             throw new ServiceException(e);
         }
+    }
+    
+    private void fillProjectDetailsPage(ProjectEntity projectEntity, XSSFSheet projectSheet) {
+        Row nameRow = projectSheet.createRow(0);
+        Cell nameLabelCell = nameRow.createCell(0);
+        nameLabelCell.setCellValue("Project name:");
+        
+        Cell nameCell = nameRow.createCell(1);
+        nameCell.setCellValue(projectEntity.getName());
+        
+        Row shortDescriptionRow = projectSheet.createRow(1);
+        Cell shortDescriptionLabelCell = shortDescriptionRow.createCell(0);
+        shortDescriptionLabelCell.setCellValue("Project short description:");
+        
+        Cell shortDescriptionCell = shortDescriptionRow.createCell(1);
+        shortDescriptionCell.setCellValue(projectEntity.getShortDescription());
+        
+        Row longDescriptionRow = projectSheet.createRow(2);
+        Cell longDescriptionLabelCell = longDescriptionRow.createCell(0);
+        longDescriptionLabelCell.setCellValue("Project long description:");
+        
+        Cell longDescriptionCell = longDescriptionRow.createCell(1);
+        longDescriptionCell.setCellValue(projectEntity.getLongDescription());
+        
+        projectSheet.setColumnWidth(0, 10000);
+        projectSheet.setColumnWidth(1, 10000);
+    }
+    
+    private void setSheetHeader(XSSFSheet sheet) {
+        sheet.setColumnWidth(0, 2000);
+        sheet.setColumnWidth(1, 8000);
+        sheet.setColumnWidth(2, 10000);
+        sheet.setColumnWidth(3, 2000);
+        sheet.setColumnWidth(4, 10000);
+        sheet.setColumnWidth(5, 10000);
+        sheet.setColumnWidth(6, 6000);
+    }
+    
+    private void fillMainRow(Row mainRow, CellStyle style) {
+        Cell cell0 = mainRow.createCell(0);
+        cell0.setCellValue(CASE_ID);
+        cell0.setCellStyle(style);
+        
+        Cell cell1 = mainRow.createCell(1);
+        cell1.setCellValue(CASE_NAME);
+        cell1.setCellStyle(style);
+        
+        Cell cell2 = mainRow.createCell(2);
+        cell2.setCellValue(CASE_DESCRIPTION);
+        cell2.setCellStyle(style);
+        
+        Cell cell3 = mainRow.createCell(3);
+        cell3.setCellValue(STEP_NUMBER);
+        cell3.setCellStyle(style);
+        
+        Cell cell4 = mainRow.createCell(4);
+        cell4.setCellValue(STEP_DESCRIPTION);
+        cell4.setCellStyle(style);
+        
+        Cell cell5 = mainRow.createCell(5);
+        cell5.setCellValue(STEP_EXPECTED_RESULT);
+        cell5.setCellStyle(style);
+
+        Cell cell6 = mainRow.createCell(6);
+        cell6.setCellValue(STEP_COMMENT);
+        cell6.setCellStyle(style);
+    }
+    
+    private void createStepRows(Row row, CellStyle borderStyle, StepEntity stepEntity) {
+        Cell stepNumberCell = row.createCell(3);
+        stepNumberCell.setCellValue(stepEntity.getOrderNumber());
+        stepNumberCell.setCellStyle(borderStyle);
+        
+        Cell stepDescriptionCell = row.createCell(4);
+        stepDescriptionCell.setCellValue(stepEntity.getDescription());
+        stepDescriptionCell.setCellStyle(borderStyle);
+        
+        Cell stepExpectedResultCell = row.createCell(5);
+        stepExpectedResultCell.setCellValue(stepEntity.getExpectedResult());
+        stepExpectedResultCell.setCellStyle(borderStyle);
+        
+        Cell stepCommentCell = row.createCell(6);
+        if (stepEntity.getComment() != null) {
+            stepCommentCell.setCellValue(stepEntity.getComment());
+            stepCommentCell.setCellStyle(borderStyle);
+        } else {
+            stepCommentCell.setCellValue("-");
+            stepCommentCell.setCellStyle(borderStyle);
+        } 
     }
 
 }
